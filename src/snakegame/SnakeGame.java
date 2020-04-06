4616bb41903661affbf42d1cafc5700a08cb867f
VIJAY GUPTA <vijaygupta131999@gmail.com>
2020-04-06 22:18:38 +0530

    Built a frame UI

--- src/snakegame/SnakeGame.java
+++ src/snakegame/SnakeGame.java
@@ -1,5 +1,7 @@
 
 package snakegame;
+import java.awt.Color;
+import javax.swing.JFrame;
 
 /**
  *
@@ -9,7 +11,14 @@ public class SnakeGame {
 
     
     public static void main(String[] args) {
-       
+        JFrame frame=new JFrame();
+        
+        frame.setBounds(100, 100, 905, 700);
+        frame.setBackground(Color.yellow);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
     }
     
 }
